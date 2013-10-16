ccad00af864b8aa8015d1cf94ca5857a5e81daf7
Bas Kamer <bas@bushbaby.nl>
2013-10-16 15:35:36 +0200

    copies text_domain from translator configuration were applicable

--- src/BsbDoctrineTranslator/Factory/ManagerFactory.php
+++ src/BsbDoctrineTranslator/Factory/ManagerFactory.php
@@ -19,6 +19,15 @@ class ManagerFactory implements FactoryInterface
 
         $config     = isset($config['bsb_doctrine_translator']['manager']) ? $config['bsb_doctrine_translator']['manager'] : array();
 
+        // copies text_domains from translator remote_translation
+        if (isset($config['translator']['remote_translation']) && is_array($config['translator']['remote_translation'])) {
+            foreach($config['translator']['remote_translation'] as $remote_translation) {
+                if ($remote_translation['type'] == 'BsbDoctrineTranslationLoader') {
+                    $config['bsb_doctrine_translator']['manager']['text_domains'][] = $remote_translation['text_domain'];
+                }
+            }
+        }
+
         $service = new DoctrineTranslatorService($loader, $em, $config);
 
         return $service;
